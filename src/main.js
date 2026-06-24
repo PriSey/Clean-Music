a7de8fdb4857e118352706643c82072eb7e99d1b
Seysha <PSPuttaguna@gmail.com>
2026-06-24 05:18:19 -0400

    ui updates

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ let greetMsgEl;
 let folderSelectButton;
 let lbd;
 let song_disp;
+let pos_interaction = false;
 
 listen("LeftbarDirButtons", (event) => {
 
@@ -37,7 +38,10 @@ listen("CreateSongButtons", (event) => {
   play.classList.add("PButton")
   play.classList.add("PQButton")
   play.dataset.path = event.payload.id;
-  play.innerText = "play";
+  let play_img = document.createElement("img")
+  play_img.src = "./assets/play.svg"
+  play_img.classList.add("icon");
+  play.append(play_img);
 
   play.addEventListener("click", async function(clickEvent) {
     let inner_path = clickEvent.target.dataset.path;
@@ -48,7 +52,10 @@ listen("CreateSongButtons", (event) => {
   queue.classList.add("QButton")
   queue.classList.add("PQButton")
   queue.dataset.path = event.payload.id;
-  queue.innerText = "queue"
+  let queue_img = document.createElement("img")
+  queue_img.src = "./assets/queue.svg"
+  queue_img.classList.add("icon");
+  queue.append(queue_img);
 
   queue.addEventListener("click", async function(clickEvent) {
     let inner_path = clickEvent.target.dataset.path;
@@ -83,14 +90,21 @@ async function directoryButtonFunction(clickEvent){
   folder_name.classList.add("left_disp");
 
   let play = document.createElement("button");
-  play.innerText = "play";
   play.dataset.path = target_path;
   play.classList.add("PButton")
   play.classList.add("PQButton")
+  let play_img = document.createElement("img")
+  play_img.src = "./assets/play.svg"
+  play_img.classList.add("icon");
+  play.append(play_img);
 
 
   let queue = document.createElement("button");
-  queue.innerText = "queue";
+  let queue_img = document.createElement("img")
+  queue_img.src = "./assets/queue.svg"
+  queue_img.classList.add("icon");
+  queue.append(queue_img);
+
   queue.dataset.path = target_path;
   queue.classList.add("QButton");
   queue.classList.add("PQButton")
@@ -153,11 +167,65 @@ async function pickDirectory(btn){
   await invoke("load_from_music_folder", {path: file})
 }
 
+async function updateCurrentPlaying(){
+  let is_playing = await invoke("get_up");
+  let current_playing_disp = document.getElementById("current-playing");
+  let song_title_disp = current_playing_disp.querySelector("#song-title");
+  if (is_playing){
+    let song_title = await invoke("get_current_song");
+    song_title_disp.innerText = song_title;
+  }
+  else{
+    song_title_disp.innerText = ""
+  }
+
+}
+
 document.addEventListener("click", async function(event){
   if(event.target.classList.contains("actionButton")){
-      await invoke("send_command", {cmd: event.target.id});
+      let cmd = Number(event.target.id.at(-1));
+      if (event.target.classList.contains("changer")){
+        let intended = Number(event.target.id.at(-2));
+        let new_id_number = intended;
+        let inner_img = event.target.querySelector(".ActionButtonImg");
+        if (intended == cmd){
+          new_id_number += 1;
+          inner_img.src = inner_img.dataset.image2;
+        } else {
+          inner_img.src = inner_img.dataset.image1;
+        }
+        event.target.id = "a" + intended + new_id_number;
+      }
+      await invoke("send_command", {cmd: cmd});
   }
 });
+
+async function updatePosition(range){
+  let is_playing = await invoke("get_up");
+  if (is_playing && !pos_interaction){
+    range.disabled = false;
+    range.value = await invoke("get_position");
+  } else if (is_playing && pos_interaction){
+      range.disabled = false;
+  } else {
+    range.value = 0;
+    range.disabled = true;
+  }
+}
+
+document.getElementById("position").addEventListener("pointerdown", function() {
+  pos_interaction = true;
+});
+
+document.getElementById("position").addEventListener("pointerup", function() {
+  pos_interaction = false;
+});
+
+document.getElementById("position").addEventListener("change", async function(e) {
+  let position = e.target.valueAsNumber;
+  await invoke("seek_position", {virtualPosition: position});
+});
+
 
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
@@ -170,10 +238,15 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   let volume_slider = document.getElementById("volume");
-  volume_slider.addEventListener("change", async function(event){
+  volume_slider.addEventListener("input", async function(event){
     let volume = volume_slider.value;
     await invoke("set_volume", {volume: volume});
   });
+
+  setInterval(function() {
+      updatePosition(document.getElementById("position"));
+      updateCurrentPlaying();
+  },100);
 
 });
 
